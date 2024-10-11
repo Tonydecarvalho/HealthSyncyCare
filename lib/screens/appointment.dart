f449225e33f14990e6e0ee6ac022c900f8401b41
Blazej3 <b.goszczyn88@gmail.com>
2024-10-11 11:26:58 +0300

    appointment page update

--- lib/screens/appointment.dart
+++ lib/screens/appointment.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:intl/intl.dart';
 
 class AppointmentPage extends StatefulWidget {
-  final String userId; // User ID as a parameter
+  final String userId;
 
-  const AppointmentPage({super.key, required this.userId}); // Constructor
+  const AppointmentPage({super.key, required this.userId});
 
   @override
   _AppointmentPageState createState() => _AppointmentPageState();
@@ -18,8 +17,9 @@ class _AppointmentPageState extends State<AppointmentPage> {
   DateTime? _selectedDay;
   String? _selectedTime;
   bool _isSaving = false;
-  bool _isLoading = true; // Loading indicator
+  bool _isLoading = true;
   final Map<DateTime, List<Map<String, dynamic>>> _availableTimes = {};
+  final TextEditingController _symptomsController = TextEditingController();
 
   @override
   void initState() {
@@ -27,72 +27,115 @@ class _AppointmentPageState extends State<AppointmentPage> {
     _fetchAvailableTimes();
   }
 
-  Future<void> _fetchAvailableTimes() async {
-    setState(() => _isLoading = true); // Start loading
-    try {
-      final userDoc = FirebaseFirestore.instance.collection('User').doc(widget.userId);
-      final appointmentsSnapshot = await userDoc.collection('appointment').get();
+Future<void> _fetchAvailableTimes() async {
+  setState(() => _isLoading = true);
+  try {
+    final appointmentsSnapshot = await FirebaseFirestore.instance
+        .collection('BookAppointments')
+        .get();
 
-      for (var doc in appointmentsSnapshot.docs) {
-        final data = doc.data();
-        final appointmentDate = (data['appointmentDate'] as Timestamp).toDate();
-        final isBooked = true;
+    for (var doc in appointmentsSnapshot.docs) {
+      final data = doc.data();
+      final appointmentDate = (data['appointmentDate'] as Timestamp).toDate();
+      final isBooked = data['status'] == true; // Use status directly as boolean
 
-        if (!_availableTimes.containsKey(appointmentDate)) {
-          _availableTimes[appointmentDate] = [];
-        }
-
-        _availableTimes[appointmentDate]?.add({
-          'time': appointmentDate.toLocal().toString().split(" ")[1].substring(0, 5),
-          'isBooked': isBooked,
-        });
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error fetching appointments. Please try again later: $e")),
+      final appointmentDateUTC = DateTime.utc(
+        appointmentDate.year,
+        appointmentDate.month,
+        appointmentDate.day,
       );
-    } finally {
-      setState(() => _isLoading = false); // Stop loading
-    }
-  }
 
-  Future<void> _saveAppointment() async {
-    try {
-      final userDoc = FirebaseFirestore.instance.collection('User').doc(widget.userId);
+      final appointmentTime =
+          "${appointmentDate.hour}:${appointmentDate.minute.toString().padLeft(2, '0')}";
 
-      await userDoc.collection('appointment').add({
-        'appointmentDate': Timestamp.fromDate(_selectedDay!),
-        'time': _selectedTime,
-        'status': 'booked',
-      });
-
-      final timesForDay = _availableTimes[DateTime.utc(
-          _selectedDay!.year,
-          _selectedDay!.month,
-          _selectedDay!.day)];
-      final timeIndex = timesForDay!
-          .indexWhere((time) => time['time'] == _selectedTime);
-      if (timeIndex != -1) {
-        timesForDay[timeIndex]['isBooked'] = true;
+      if (!_availableTimes.containsKey(appointmentDateUTC)) {
+        _availableTimes[appointmentDateUTC] = [];
       }
 
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Appointment saved and marked as booked!')),
-      );
-
-      setState(() {
-        _isSaving = false;
+      _availableTimes[appointmentDateUTC]!.add({
+        'time': appointmentTime,
+        'isBooked': isBooked,
       });
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error saving appointment. Please try again later: $e")),
-      );
     }
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Error fetching appointments: $e")),
+    );
+  } finally {
+    setState(() => _isLoading = false);
+  }
+}
+
+
+Future<void> _saveAppointment(String symptoms) async {
+  try {
+    DateTime appointmentDateTime = DateTime(
+      _selectedDay!.year,
+      _selectedDay!.month,
+      _selectedDay!.day,
+      int.parse(_selectedTime!.split(':')[0]),
+      int.parse(_selectedTime!.split(':')[1]),
+    );
+
+    // Add new appointment to the database
+    await FirebaseFirestore.instance.collection('BookAppointments').add({
+      'appointmentDate': Timestamp.fromDate(appointmentDateTime),
+      'status': true,  // Marking as booked
+      'symptoms': symptoms,
+      'patientId': widget.userId,
+      'createdTime': Timestamp.now(),
+    });
+
+    // Show a confirmation message
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Appointment saved and marked as booked!')),
+    );
+
+    // Refresh the available times to reflect the newly booked appointment
+    await _fetchAvailableTimes();
+
+    setState(() {
+      _isSaving = false;
+    });
+
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Error saving appointment: $e")),
+    );
+  }
+}
+
+
+List<Map<String, dynamic>> _generateTimeSlots(DateTime day) {
+  DateTime now = DateTime.now();
+  DateTime fourWeeksLater = now.add(const Duration(days: 28));
+
+  // Ensure the day is within the next 4 weeks
+  if (day.isBefore(now) || day.isAfter(fourWeeksLater)) {
+    return []; // Return an empty list if the date is outside the 4-week window
   }
 
-  List<Map<String, dynamic>> _getAvailableTimesForDay(DateTime day) {
-    return _availableTimes[DateTime.utc(day.year, day.month, day.day)] ?? [];
+  List<Map<String, dynamic>> timeSlots = [];
+  DateTime start = DateTime(day.year, day.month, day.day, 9, 0);
+  DateTime end = DateTime(day.year, day.month, day.day, 17, 30);
+
+  while (start.isBefore(end)) {
+    final timeString =
+        "${start.hour}:${start.minute.toString().padLeft(2, '0')}";
+
+    // Check if this specific time slot is already booked using the 'isBooked' field
+    bool isBooked = _availableTimes[DateTime.utc(day.year, day.month, day.day)]?.any((time) {
+          return time['time'] == timeString && time['isBooked'] == true;
+        }) ?? false;
+
+    timeSlots.add({'time': timeString, 'isBooked': isBooked});
+    start = start.add(const Duration(minutes: 30));
   }
+  return timeSlots;
+}
+
+
+
 
   void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     setState(() {
@@ -118,7 +161,18 @@ class _AppointmentPageState extends State<AppointmentPage> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: const Text('Confirm Appointment'),
-          content: const Text('Are you sure you want to save this appointment?'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Enter your symptoms:'),
+              TextField(
+                controller: _symptomsController,
+                maxLength: 200,
+                decoration: const InputDecoration(hintText: 'Symptoms...'),
+                maxLines: 3,
+              ),
+            ],
+          ),
           actions: [
             TextButton(
               onPressed: () => Navigator.of(context).pop(),
@@ -127,7 +181,8 @@ class _AppointmentPageState extends State<AppointmentPage> {
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).pop();
-                _saveAppointment();
+                _saveAppointment(_symptomsController.text);
+                _symptomsController.clear();
               },
               style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
               child: const Text('Confirm'),
@@ -139,7 +194,11 @@ class _AppointmentPageState extends State<AppointmentPage> {
   }
 
   Widget _buildAvailableTimesGrid() {
-    if (_getAvailableTimesForDay(_selectedDay!).isEmpty) {
+    if (_selectedDay == null) return const Center(child: Text('No day selected'));
+
+    List<Map<String, dynamic>> timeSlots = _generateTimeSlots(_selectedDay!);
+
+    if (timeSlots.isEmpty) {
       return const Center(child: Text('No available times for this day.'));
     }
 
@@ -149,7 +208,7 @@ class _AppointmentPageState extends State<AppointmentPage> {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
-            'Available times for ${_selectedDay!.toLocal().day}-${_selectedDay!.toLocal().month}-${_selectedDay!.toLocal().year}',
+            'Available times for ${_selectedDay!.day}-${_selectedDay!.month}-${_selectedDay!.year}',
             style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
           ),
         ),
@@ -157,9 +216,9 @@ class _AppointmentPageState extends State<AppointmentPage> {
           child: GridView.builder(
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3, childAspectRatio: 2),
-            itemCount: _getAvailableTimesForDay(_selectedDay!).length,
+            itemCount: timeSlots.length,
             itemBuilder: (context, index) {
-              final timeData = _getAvailableTimesForDay(_selectedDay!)[index];
+              final timeData = timeSlots[index];
 
               return GestureDetector(
                 onTap: () => _onTimeSlotSelected(timeData),
@@ -167,25 +226,33 @@ class _AppointmentPageState extends State<AppointmentPage> {
                   padding: const EdgeInsets.all(1),
                   margin: const EdgeInsets.all(8),
                   decoration: BoxDecoration(
-                    color: timeData['isBooked'] ? Colors.redAccent : Colors.lightGreen,
+                    color: timeData['isBooked']
+                        ? Colors.redAccent
+                        : Colors.lightGreen,
                     border: Border.all(
-                      color: _selectedTime == timeData['time'] && !timeData['isBooked']
+                      color: _selectedTime == timeData['time'] &&
+                              !timeData['isBooked']
                           ? Colors.green.shade900
                           : Colors.transparent,
                       width: 4,
                     ),
-                    boxShadow: _selectedTime == timeData['time']
-                        ? [const BoxShadow(color: Colors.black26, blurRadius: 6)]
-                        : [],
                     borderRadius: BorderRadius.circular(12),
                   ),
                   child: Center(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Icon(timeData['isBooked'] ? Icons.lock : Icons.access_time, color: Colors.white),
+                        Icon(
+                          timeData['isBooked'] ? Icons.lock : Icons.access_time,
+                          color: Colors.white,
+                        ),
                         const SizedBox(width: 8),
-                        Text(timeData['time'], style: const TextStyle(fontSize: 18, color: Colors.white), textAlign: TextAlign.center),
+                        Text(
+                          timeData['time'],
+                          style: const TextStyle(
+                              fontSize: 18, color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
                       ],
                     ),
                   ),
@@ -208,7 +275,6 @@ class _AppointmentPageState extends State<AppointmentPage> {
           onPressed: () => _confirmAppointment(context),
           child: const Text('Save Appointment'),
           style: ElevatedButton.styleFrom(
-            iconColor: Colors.green,
             textStyle: const TextStyle(fontSize: 20),
           ),
         ),
@@ -222,7 +288,7 @@ class _AppointmentPageState extends State<AppointmentPage> {
       appBar: AppBar(
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () {}, // Add navigation logic if needed
+          onPressed: () => Navigator.pop(context),
         ),
         title: const Text('Appointment'),
       ),
@@ -237,16 +303,15 @@ class _AppointmentPageState extends State<AppointmentPage> {
                   calendarFormat: _calendarFormat,
                   selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
                   onDaySelected: _onDaySelected,
-                  onFormatChanged: (format) => setState(() => _calendarFormat = format),
-                  onPageChanged: (focusedDay) => _focusedDay = focusedDay,
+                  onFormatChanged: (format) {
+                    setState(() => _calendarFormat = format);
+                  },
+                  onPageChanged: (focusedDay) {
+                    setState(() => _focusedDay = focusedDay);
+                  },
                 ),
-                const SizedBox(height: 16.0),
-                Expanded(
-                  child: _selectedDay == null
-                      ? const Center(child: Text('Please select a day'))
-                      : _buildAvailableTimesGrid(),
-                ),
-                if (_isSaving) _buildSaveButton(),
+                Expanded(child: _buildAvailableTimesGrid()),
+                _isSaving ? _buildSaveButton() : Container(),
               ],
             ),
     );
